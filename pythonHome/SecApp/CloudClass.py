677d14a74d980fb53ddd225254015c1732364099
fflowres <fflowres@gmail.com>
2014-08-25 22:31:50 -0700

    first commit
    Gui Development

---
+++ pythonHome/SecApp/CloudClass.py
@@ -0,0 +1,264 @@
+from SecApp.FileHandlerClass import FileHandler
+from SecApp.ConfigClass import AppConfig
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import os
+import pickle
+import base64
+
+import string
+import random
+class CloudHandler:
+	'''
+
+
+
+
+	'''
+
+
+
+
+	def __init__(self):
+		self.DBConfig = AppConfig()
+		self.FH = FileHandler()
+		self.googleConfig = 'configs/gdrive.yaml'
+
+	def ssl_seed(self,size=24, chars=string.ascii_uppercase + string.digits):
+		self.randomString = ''.join(random.choice(chars) for _ in range(size))
+		return self
+
+
+	def authenticategoogle(self):
+		ga = GoogleAuth(self.googleConfig)
+		try:
+			ga.LoadClientConfig()
+			ga.LoadCredentials()
+			ga.Authorize()
+		except:
+			return ga.GetAuthUrl()
+			#code = raw_input("code: ").strip()
+		else:
+			return True
+			#auth loaded ok
+
+	def googleauthorize(self,authCode):
+		ga = GoogleAuth(self.googleConfig)
+		ga.LoadClientConfig()
+		ga.LoadCredentials()
+		code = authCode.strip()
+		ga.Auth(code)
+		ga.SaveCredentials()
+		return True
+
+	def googleupload(self,filename):
+		packageName = filename
+		ga = GoogleAuth(self.googleConfig)
+		ga.LoadClientConfig()
+		ga.LoadCredentials()
+		ga.Authorize()
+		drive = GoogleDrive(ga)
+		#flist = drive.ListFile({'q': "title contains '.crypt' and trashed = false"})
+		folderlistquery = drive.ListFile({'q': "title = 'SeccuDB' and mimeType = 'application/vnd.google-apps.folder' and trashed = false"})
+		
+
+		cloudfolder = folderlistquery.GetList()
+		if (len(cloudfolder) == 0):
+			#create folder 
+			folder = drive.CreateFile()
+			folder['title'] = "SeccuDB"
+			folder['mimeType'] = "application/vnd.google-apps.folder"
+			folder['parents'] = "root"
+			folder.Upload()
+			cloudfolder = folderlistquery.GetList()
+			if (len(cloudfolder) == 0):
+				print "error"
+				raise error('GooglePermissionsError')
+
+
+		cloudfolderid = cloudfolder[0]['id']
+		print cloudfolderid
+
+		databaseListquery = drive.ListFile({'q': "'%s' in parents and trashed = false" % (cloudfolderid)})
+		databaseList = databaseListquery.GetList()
+
+
+		database_file = drive.CreateFile()
+		database_file['title'] = packageName
+		database_file['parents']=[{"kind": "drive#fileLink" ,'id': str(cloudfolderid) }]
+		#check if already exists, if so, get id and update
+		databasenamelist = []
+		for databaseAvaliable in databaseList:
+			databasenamelist.append(databaseAvaliable['title'])
+		if packageName in databasenamelist:
+			cloudPackageQuery = drive.ListFile({'q': "title = '%s' and trashed = false" % (packageName)})
+			cloudPackage = cloudPackageQuery.GetList()
+
+			if(len(cloudPackage) > 1): # if theres more than one, go for the most recent
+				packdates = []
+				for everypack in cloudPackage:
+					packdates.append((everypack['modifiedByMeDate'],everypack['id']))
+				database_file['id'] = sorted(packdates,reverse=True)[0][1]
+
+			else:
+				database_file['id'] = cloudPackage[0]['id']
+
+		database_file.Upload()
+		return True
+
+
+	def getgooglefileid(self,title):
+
+		print os.getcwd()
+		self.ga = GoogleAuth(self.DBConfig.googleyaml)
+		self.ga.LocalWebserverAuth()
+		drive = GoogleDrive(self.ga)
+		flist = drive.ListFile({'q': "title = '%s' and trashed = false"%title})
+		files = flist.GetList() 
+		if len(files)==0:
+			return False
+		else:
+			return files[0]['id']
+
+
+	def simpleUploadGoogle(self,filename): #test_01_Files_Insert
+		drive = GoogleDrive(self.ga)
+		#f = drive.CreateFile({'fluidSecurity': parent_id})
+		file1 = drive.CreateFile()
+		file1.SetContentFile(filename) # Read local file
+		file1.Upload() # Upload it
+		
+	
+
+		return True
+	##save file ID to config
+	
+	
+	def simpleUpdateGoogle(self,filename,dbID):
+		drive = GoogleDrive(self.ga)
+	
+		file1=drive.CreateFile({'id': dbID}) #overwrite by ID
+		file1['title'] = filename
+		file1.FetchContent()
+		file1.SetContentFile(filename)
+		file1.Upload() 
+		return True
+		
+	
+	def simpleDownloadGoogle(self,filename):
+		DeleteOldFile(self.filename) #delete local version, not that safe
+	
+		drive = GoogleDrive(self.ga)
+		file1 = drive.CreateFile()
+		file1['title'] = filename
+		file1.FetchContent()  # Force download and double check content
+		return True
+	
+	
+#	
+#	
+#	def ftpDown(filename,host,user,ftpass):
+#		import ftplib
+#		session = ftplib.FTP(host,user,ftpass)
+#		file = open(filename, 'wb')  
+#		session.retrbinary('RETR %s' % filename, file.write)
+#		file.close()
+#		session.quit()
+#	
+
+
+	
+
+
+	def uploadftp(self,filename,host,user,password):	  
+
+		# upload logic
+		import ftplib
+		session = ftplib.FTP(host,user,password)
+		file = open(filename,'rb')				  # file to send
+		session.storbinary('STOR ' +filename, file)	 # send the file
+		file.close()									# close file and FTP
+		session.quit()
+
+		return True
+
+	def authenticatedropbox(self):
+		import dropbox
+
+		# Get your app key and secret from the Dropbox developer website
+		app_key = 'p2iu4n7f9yegl3u'
+		app_secret = '0903whnau7p2zde'
+
+		flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+
+		authorize_url = flow.start()
+		print "============= authenticate dropbox" + str(authorize_url)
+
+		pickled  = pickle.dumps(flow)
+		encodedPickle = base64.b64encode(pickled)
+		self.DBConfig.putmap('cloudinfo','dropboxobject', str(encodedPickle) )
+
+		return authorize_url
+
+
+
+	def uploaddropbox(self,filename,authcode):
+		import socket
+		socket.RAND_add(self.ssl_seed().randomString, 75.0)
+		import dropbox
+
+		try:
+			# Get your app key and secret from the Dropbox developer website
+			# encodedPickle =self.DBConfig.mapget('cloudinfo')['dropboxobject']
+			# decodedPickle = base64.b64decode(encodedPickle)
+			# flow = pickle.loads(decodedPickle)
+
+			app_key = 'p2iu4n7f9yegl3u'
+			app_secret = '0903whnau7p2zde'
+
+			flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+
+			authorize_url = flow.start()
+			print "============= authenticate dropbox" + str(authorize_url)
+
+
+			print "loaded ok"
+			print "------ authcode: " + str(authcode)
+			access_token, user_id = flow.finish(authcode)
+
+			print "finidhed oko"
+			client = dropbox.client.DropboxClient(access_token)
+			print "finidhed client"
+			print 'linked account: ', client.account_info()
+
+			#Uploading files
+			f = open(filename, 'rb')
+			response = client.put_file('/'+str(filename), f)
+			print "uploaded:", response
+
+		except Exception as e:
+			print("Upload failed: " + str(e))
+			return False
+		return True
+
+	def uploadgoogledrive(self,filename):
+		googlefileid = self.getgooglefileid(filename)
+		if googlefileid == False:
+			self.simpleUploadGoogle(filename)
+		else:
+			self.simpleUpdateGoogle(filename,googlefileid)
+		return True
+
+	def uploadicloud(self,filename,email,password):
+
+		return True
+
+	def uploadskydrive(self,filename,email,password):	
+
+		return True
+
+	
+
+
+
---
+++ pythonHome/SecApp/SyncClass.py
@@ -0,0 +1,222 @@
+from SecApp.FileHandlerClass import FileHandler
+from SecApp.ConfigClass import AppConfig
+from SecApp.CloudClass import CloudHandler
+from SecApp.IOClass import AppIO
+import socket
+#ssl seed
+import string
+import random
+
+
+class Sync:
+	'''
+	import scan: scans existing self.db and rebuilds config file 
+	create self.db: creates self.db file, master index, question index and table index
+
+
+
+	'''
+
+
+
+
+	def __init__(self):
+		self.FH = FileHandler()
+		self.DBConfig = AppConfig()
+		
+	def ssl_seed(self,size=24, chars=string.ascii_uppercase + string.digits):
+		self.randomString = ''.join(random.choice(chars) for _ in range(size))
+		return self
+
+
+
+
+	def getcloudselection(self):
+		try:
+			
+			backupLocation = self.DBConfig.mapget('cloudinfo')['location']
+			validlocation = ['ftp' , 'googledrive' , 'icloud' , 'dropbox' , 'skydrive']
+			if (backupLocation in validlocation):
+				self.backup = backupLocation
+				return True
+			else:
+				self.backup = "False"
+				return False
+		except KeyError:
+
+			self.backup = "False"
+			return False
+
+
+
+
+
+	def parseconfigjson(self,json):
+		self.uploadconfig = eval(json)
+
+		print "----------- VALUE: " + str(self.uploadconfig['action'])
+		if (self.uploadconfig['action'] == 'upload'):
+			status = self.upload()
+
+		if (self.uploadconfig['action'] == 'authenticate'):
+			status = self.authenticate()
+
+		if (self.uploadconfig['action'] == 'authorize'):
+			status = self.authorize()
+
+		if (self.uploadconfig['action'] == 'save'):
+			status = self.setbackuplocation()
+
+		if (self.uploadconfig['action'] == 'import'):
+			#status = self.importDB()
+			pass
+		return status
+
+	def setbackuplocation(self): # data = {location: xxxx , username: xxxxx , password: xxxxx}
+		backupconfig = self.uploadconfig.copy()
+		backupconfig.pop('action')
+		try:
+			backupconfig.pop('dbpassword')
+		except KeyError:
+			pass
+
+		for setting in backupconfig.keys():
+			if (setting in ("location","ftphost","ftpuser","gmail","appleid","dropboxid","livemail")):
+				self.DBConfig.putmap('cloudinfo', setting , backupconfig[setting])
+
+
+		
+
+
+	def getconfig(self,location=None):
+		self.loc = location
+
+		if (self.loc==None):
+			self.loc = self.backup
+			if (self.loc=="False"):
+				print "need location"
+				return False
+
+
+		try:
+			if (self.loc == 'ftp'):
+				Host=self.DBConfig.mapget('cloudinfo')['ftphost']
+				User=self.DBConfig.mapget('cloudinfo')['ftpuser']
+				return str(dict(ftphost=Host,ftpuser=User))
+	
+			if (self.loc == 'googledrive'):
+
+				return True
+	
+			if (self.loc == 'icloud'):
+				Email=self.DBConfig.mapget('cloudinfo')['appleid']
+				return str(dict(appleid=Email))
+	
+			if (self.loc == 'dropbox'):
+				Email=self.DBConfig.mapget('cloudinfo')['dropboxid']
+				return str(dict(dropboxid=Email))
+	
+			if (self.loc == 'skydrive'):
+				Email=self.DBConfig.mapget('cloudinfo')['livemail']
+				return str(dict(livemail=Email))
+
+			else:
+				return False
+		except KeyError: 
+			return False
+
+	def authenticate(self):
+		socket.RAND_add(self.ssl_seed().randomString, 75.0) # pre-seed generator
+		if (self.uploadconfig['location'] == 'dropbox'):
+
+			url = CloudHandler().authenticatedropbox()
+			print "============= authenticate dropbox" + str(url)
+			return url
+		if (self.uploadconfig['location'] == 'googledrive'):
+
+			url = CloudHandler().authenticategoogle()
+			print "============= authenticate google" + str(url)
+			if (url != True):
+				return url
+			else:
+				return True # then upload
+
+	def authorize(self):
+		if (self.uploadconfig['location'] == 'googledrive'):
+			socket.RAND_add(self.ssl_seed().randomString, 75.0) # pre-seed generator
+			authcode = self.uploadconfig['authcode']
+			status = CloudHandler().googleauthorize(authcode)
+			if (status): # True
+				return True
+			else:
+				return False
+
+
+	def upload(self):
+		
+		self.key = self.uploadconfig["dbpassword"]
+
+		socket.RAND_add(self.ssl_seed().randomString, 75.0) # pre-seed generator
+		self.FH.genpack(self.key) #packadge database
+
+		self.packname = self.FH.finalpackname #from config
+
+
+		if (self.uploadconfig['location'] == 'ftp'):
+			host = self.uploadconfig['ftphost']
+			user = self.uploadconfig['ftpuser']
+			password = self.uploadconfig['password']
+
+			self.DBConfig.putmap('cloudinfo','location','ftp')
+			self.DBConfig.putmap('cloudinfo','ftphost', host )
+			self.DBConfig.putmap('cloudinfo','ftpuser', user )
+			status = CloudHandler().uploadftp(self.packname, host, user, password)
+
+		if (self.uploadconfig['location'] == 'googledrive'):
+			self.DBConfig.putmap('cloudinfo','location','googledrive')
+			status = CloudHandler().uploadgoogledrive(self.packname)
+
+		if (self.uploadconfig['location'] == 'icloud'):
+			email = self.uploadconfig['appleid']
+			password = self.uploadconfig['password']
+
+			self.DBConfig.putmap('cloudinfo','location','icloud')
+			self.DBConfig.putmap('cloudinfo','appleid',email)
+			status = uploadicloud(self.packname, email, pasword)
+
+		if (self.uploadconfig['location'] == 'dropbox'):
+			authcode = self.uploadconfig['authcode']
+
+			self.DBConfig.putmap('cloudinfo','location','dropbox')
+			# self.DBConfig.putmap('cloudinfo','dropboxid',authcode)
+			status = CloudHandler().uploaddropbox(self.packname, authcode)
+
+		if (self.uploadconfig['location'] == 'skydrive'):
+			email = self.uploadconfig['livemail']
+			password = self.uploadconfig['password']
+
+			self.DBConfig.putmap('cloudinfo','location','googledrive')
+			self.DBConfig.putmap('cloudinfo','livemail',email)
+			status = uploadskydrive(self.packname, email, password)
+		#print self.FH.deletefile(str(self.FH.finalpackname)) # clean-up
+		try:
+			import os
+			os.remove(self.FH.finalpackname)
+			#except OSError:
+		except Exception as e:
+			print e
+			ret =  "upload success: " + str(status) + " [ERROR, Clean-up]: " + str(e)
+			return ret
+
+		else:
+		  return True
+
+	
+
+	
+
+	 
+
+
+
+
